aba35d0ed0ce63c053a2826945a518c5005f9f9d
Tobias Zarlenga <tobiazarlenga@gmail.com>
2024-04-22 09:22:39 -0300

    termine el punto 1

--- puntos.js/punto1.js
+++ puntos.js/punto1.js
@@ -1,1 +1,7 @@
-let hola
+let month = ['enero', 'Febrero', 'Marzo', 'Abril','mayo' , 'junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+document.write('<h1> lista de los meses</h1>');
+document.write('<ul>');
+for(let i = 0; i < month.length;i++) {
+    document.write(`<li>${month[i]}</li>`);
+}
+    document.write('</ul>');    
